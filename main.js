d67d3f1b321208b6119ecb76e5bca5caa8684177
Esther Jeon <estherjeon@Esthers-MacBook-Air.local>
2023-12-04 13:48:33 -0800

    hamburger menu

---
+++ main.js
@@ -0,0 +1,8 @@
+function openMenu() {
+    document.getElementById("mobile-nav").style.display = "block";
+}
+
+function closeMenu() {
+    document.getElementById("mobile-nav").style.display = "none";
+}
+
