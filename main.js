c25ea90dc55f10140fdbfd6f9f0b60267881ac8f
Soonho Kwon <skwon@soonhos-mbp.macromedia.com>
2024-01-11 12:13:36 -0800

    nav hover + current states. modal progress

--- main.js
+++ main.js
@@ -5,3 +5,38 @@ function openMenu() {
 function closeMenu() {
     document.getElementById("mobile-nav").style.display = "none";
 }
+
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+
+// // Get the modal
+// var modal = document.getElementById("susieModal");
+              
+// // Get the button that opens the modal
+// var btn = document.getElementById("susieBtn");
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks the button, open the modal 
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
